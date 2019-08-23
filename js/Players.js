58318dd23f4567fdfb84ddc5bff38c917c6aaf6d
Matthew McQuain <matt.mcquain@gmail.com>
2019-08-22 21:03:29 -0700

    added a createTokens method to our Player class

--- js/Players.js
+++ js/Players.js
@@ -4,6 +4,22 @@ class Players {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.tokens = [];
+        this.tokens = this.createTokens(21);
+    }
+
+    /** 
+    * Creates the token objects for our players
+    * @param {integer} num - Number of token objects to be created 
+    * @returns {array} an array of the newly created token objects
+    */
+    createTokens(num) {
+        const tokens = [];
+
+        for (let i = 0; i < num; i++) {
+            let token = new Tokens(i, this);
+            tokens.push(token);
+        }
+    
+        return tokens;
     }
 }
