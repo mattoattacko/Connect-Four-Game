4fe036ac2e02ad6c27f233526b7e6237a5ecdac8
Matthew McQuain <matt.mcquain@gmail.com>
2019-08-24 19:06:24 -0700

    creatPlayers method added to the Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 class Game {
-    // fields, constructors, and methods
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
+
+    /**
+     * Creates two player objects
+     * @return {array}  an array of two player objects
+     */
+    createPlayers() {
+        const players = [new Players('Player One', 1, '#e15258', true),
+                         new Players('Player Two', 2, '#e59a13')];
+
+        return players;
+    }
 }
