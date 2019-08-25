a118dca1e5b66e1e7501a5f1263d51b99ab73629
Matthew McQuain <matt.mcquain@gmail.com>
2019-08-24 17:44:02 -0700

    added createSpaces method to the Board class

--- js/Board.js
+++ js/Board.js
@@ -2,7 +2,7 @@ class Board {
     constructor() {
         this.rows = 6;
         this.columns = 7;
-        this.spaces = createSpaces; 
+        this.spaces = this.createSpaces(); 
     }
 
     /** 
