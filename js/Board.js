c4da6120b97b4cc65fa963a5550a58c82d95988e
Matthew McQuain <matt.mcquain@gmail.com>
2019-08-24 17:29:07 -0700

    added createSpaces method to the Board class

--- js/Board.js
+++ js/Board.js
@@ -2,6 +2,27 @@ class Board {
     constructor() {
         this.rows = 6;
         this.columns = 7;
-        this.spaces = []; 
+        this.spaces = createSpaces; 
+    }
+
+    /** 
+     * Generates a 2D array of spaces
+     * @return {array} An array of space objects
+     */
+    createSpaces() {
+        const spaces = [];
+
+        for(let x = 0; x < this.columns; x++){
+            const column = [];
+
+            for (let y = 0; y < this.rows; y++){
+                const space = new Spaces(x,y);
+                column.push(space);
+            }
+
+            spaces.push(column);
+        }
+
+        return spaces;
     }
 }
