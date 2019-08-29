84d883ff0e3d9f330a8fcb09a3706c69e7801a78
Matthew McQuain <matt.mcquain@gmail.com>
2019-08-28 21:15:32 -0700

    more work on Token

--- js/Tokens.js
+++ js/Tokens.js
@@ -9,29 +9,21 @@ class Tokens {
      * Draws new HTML token.
      */
     drawHTMLToken() {
+        //Create a div element and save it to a variable.
         const token = document.createElement('div');
+        // Append this element to the existing HTML element with the id of "game-board-underlay".
         document.getElementById('game-board-underlay').appendChild(token);
+        // Using the DOM method, "setAttribute()", add an "id" attribute to the newly created element, with a value equal to the Token object's "id" property. This is how our visual representation of the tokens - the HTML elements - will be tied to the JavaScript Token objects.
         token.setAttribute('id', this.id);
+        // Using the DOM method, "setAttribute()", add a "class" attribute to the newly created element, with a value equal to the word "token". The CSS included for this project already has the styles coded for any HTML element with a class of "token".
         token.setAttribute('class', 'token');
+        // Set the background color of the token element equal to the Token object's owner's color property.
         token.style.backgroundColor = this.owner.color;
     }
 }
 
-/**
- * Add a method called drawHTMLToken() to the Token class using the requirements and guidelines below.
- * Add a getter method, htmlToken() to the Token class using the requirements and guidelines below.
- */
 
-/**
- * "drawHTMLToken()" method...
- * Create a div element and save it to a variable.
- * Append this element to the existing HTML element with the id of "game-board-underlay".
- * Using the DOM method, "setAttribute()", add an "id" attribute to the newly created element, with a value equal to the Token object's "id" property. This is how our visual representation of the tokens - the HTML elements - will be tied to the JavaScript Token objects.
- * Using the DOM method, "setAttribute()", add a "class" attribute to the newly created element, with a value equal to the word "token". The CSS included for this project already has the styles coded for any HTML element with a class of "token".
- * Set the background color of the token element equal to the Token object's owner's color property.
- */
 
- /**
  * 
  * Getter method "htmlToken()"...
  * should return the HTML token element associated with the Token object.
