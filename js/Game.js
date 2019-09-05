ecc994b52a2ed097dfe9867be0e8584794866400
Matto <matt.mcquain@gmail.com>
2019-09-04 20:45:03 -0700

    added getter methods for unusedTokens, activePlayers, and activeToken

--- js/Game.js
+++ js/Game.js
@@ -6,6 +6,17 @@ class Game {
     }
 
     /**
+     * Returns active player
+     * @return {Object} - Player = the active player
+     * We use array method 'find' instead of 'filter'. 'Filter' would return an array of every element that passes the provided test, and since we are only looking for a single value here, we use 'find' to simply return the first/only element that passes the test.
+     * Our test condition here is wether or not the Player objects active property = true?
+     * We call 'find' on the Game objects 'players' property which holds an array of the games players.
+     */
+    get activePlayer() {
+        return this.players.find(player => player.active);
+    }
+
+    /**
      * Creates two player objects
      * @return {array}  an array of two player objects
      */
