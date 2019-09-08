ac550da683d61648a0b2533632fafcf84cad2d1f
Matthew McQuain <matt.mcquain@gmail.com>
2019-09-07 18:23:38 -0700

    fixed branching issue

--- js/Players.js
+++ js/Players.js
@@ -12,14 +12,50 @@ class Players {
     * @param {integer} num - Number of token objects to be created 
     * @returns {array} an array of the newly created token objects
     */
-    createTokens(num) {
-        const tokens = [];
+   createTokens(num) {
+    const tokens = [];
 
-        for (let i = 0; i < num; i++) {
-            let token = new Tokens(i, this);
-            tokens.push(token);
-        }
-    
-        return tokens;
+    for (let i = 0; i < num; i++) {
+        let token = new Token(i, this);
+        tokens.push(token);
+    }
+
+    return tokens;
+}
+
+    /**
+     * Gets all tokens that have not been dropped yet.
+     * @return {array} - Array of unused tokens.
+     */
+    get unusedTokens() {
+        return this.tokens.filter(token => !token.dropped);
+    }
+
+    /**
+     * Gets the active token by returning the first token in the array of unused tokens.
+     * @return {Object} - First token object in the array of unused tokens.
+     */
+    get activeToken() {
+        return this.unusedTokens[0];
+    }
+
+    /**
+     * Gets all tokens that have not been dropped
+     * @return {array} - Array of unused tokens
+     * The filter method is called on an array and is passed to test, and returns another array any elements from the original array that passed our test, which is any token property that does not equal true.
+     * We are calling it on the players tokens property, which is holding an array of every token object belonging to the player.
+     * Could also use a for loop to find the non-dropped tokens and pushed them onto an array.
+     */
+    get unusedTokens() {
+        return this.tokens.filter(token => !token.dropped);
+    }
+
+    /**
+     * Gets the active token by returning the first token in the array of unused tokens.
+     * @return {Object} - First token object in the array of unusedTokens array we just created.
+     * Everytime the player takes a turn, our getter method grabs the first token object in this array.
+     */
+    get activeToken() {
+        return this.unusedTokens[0];
     }
 }
